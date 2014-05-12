f79b7ca77950da6df281225d5bb401deb929cb19
PapinPar <69Knocker@gmail.com>
2014-05-12 23:51:48 +0300

    12/05/14
    Signed-off-by: PapinPar <69Knocker@gmail.com>

--- src/SimpleBanner.java
+++ src/SimpleBanner.java
@@ -12,39 +12,100 @@ import java.util.ArrayList;
 
 import javax.swing.JApplet;
 import javax.swing.JButton;
+import javax.swing.JPanel;
 
 public class SimpleBanner extends JApplet implements ActionListener {
 	ArrayList<JButton> buttons = new ArrayList<JButton>();
 	JButton button;
 	Color red;
 	Container cp = getContentPane();
+	int col =3 ;
+	int row =3 ;
 	  public void init() 
 	  {
-		  int col =2 ;
-		  int row =3 ;
-		cp.setLayout(new GridLayout(row,col));
-	    for(int i = 1; i <=row*col; i++)
+		  JPanel jp= new JPanel();
+		  String name[]={"a","b","c","d","e"};
+		  int x[] = {0,400,200,300,500};
+		  int y[] = {0,200,200,400,500};
+		  jp.setLayout(null);
+		  for(int i =0;i< 5;i++){
+			  jp.add(getSector(x[i],y[i], 200, 200,name[i] ,3,3));
+		  }
+		  /*
+		  JPanel jp= new JPanel();
+		  JPanel jp1= new JPanel();
+		  JPanel jp2= new JPanel();
+
+		  jp1.setBackground(Color.BLUE);	
+		  jp1.setBounds(100, 100, 350, 350);
+
+		  jp2.setBackground(Color.BLUE);	
+		  jp2.setBounds(500, 500, 350, 350);
+		  jp.add(jp2);
+		  jp.add(jp1);
+		  
+		  jp.setLayout(null);
+		  jp1.setLayout(new GridLayout(row,col));
+		  for(int i = 1; i <=row*col; i++)
 	    	{
-	    		button =new JButton("Место "+i);
+	    		button =new JButton("A"+i);
 	    		buttons.add(button);
-	    		cp.add(button);
+	    		jp1.add(button);
 	    		button.setBackground(Color.green);
-				button.addActionListener(this);
-				
+				button.addActionListener(this);				
 	    	
 	    	}
-	    
+		  jp2.setLayout(new GridLayout(row,col));
+		  for(int i = 1; i <=row*col; i++)
+	    	{
+	    		button =new JButton("B"+i);
+	    		buttons.add(button);
+	    		jp2.add(button);
+	    		button.setBackground(Color.green);
+				button.addActionListener(this);				
+	    	
+	    	}*/
+		  cp.add(jp);
+	  }	  
+	  
+	  private JPanel getSector(int x,int y,int width, int height,String name,int row, int col) 
+	  {
+		  JPanel jp= new JPanel();
+
+		  jp.setBackground(Color.BLUE);	
+		  jp.setBounds(x, y, width, height);
+		  jp.setLayout(new GridLayout(row,col));
+		  for(int i = 1; i <=row*col; i++)
+	    	{
+	    		button =new JButton(name+i);
+	    		buttons.add(button);
+	    		jp.add(button);
+	    		button.setBackground(Color.green);
+				button.addActionListener(this);				
+	    	
+	    	}
+		  return jp;
 	  }
+	  
+	  
 	  public void actionPerformed(ActionEvent e) 
 	  {			
+		 StringBuffer string = new StringBuffer(e.getActionCommand());
+		 System.out.print(string);
+		 String sector = new String(""+string.charAt(0));
+		 string =	 string.deleteCharAt(0);
 		 
-		 StringBuffer buffer = new StringBuffer(e.getActionCommand());
-		 String string = buffer.substring(6);
-		 JButton button = buttons.get(Integer.parseInt(string)-1);
-		 button.setBackground(Color.red);
-		// System.out.println(string);
-	
-		  
+		 if (sector.endsWith("a"))
+		 {
+			 JButton button = buttons.get(Integer.parseInt(string.toString())-1);
+			 button.setBackground(Color.red);
+			 System.out.println("sector = "+sector);
+		 }
+		 else
+		 {
+			 JButton button = buttons.get((row*col)+Integer.parseInt(string.toString())-1);	
+			 button.setBackground(Color.red);		 
+		 }
 	  }
 	  public static void main(String[] args) {
 		  
